86fc26a27495b1487a2056ba5c03bc278294f770
kwonsss <sh.kwon@dhandy.co.kr>
2024-04-11 12:01:06 +0900

    Radzen Skeleton

---
+++ RadzenSkeleton/Program.cs
@@ -0,0 +1,34 @@
+using Radzen;
+using RadzenSkeleton.Components;
+
+namespace RadzenSkeleton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddRadzenComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
