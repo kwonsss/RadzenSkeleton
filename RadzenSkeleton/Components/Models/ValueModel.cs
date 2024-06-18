58c5d912175953518335d045a1f54bce153cabcf
kwonsss <sh.kwon@dhandy.co.kr>
2024-06-18 11:24:25 +0900

    DataGrid InCell Editing

---
+++ RadzenSkeleton/Components/Models/ValueModel.cs
@@ -0,0 +1,24 @@
+﻿namespace RadzenSkeleton.Components.Models
+{
+    public class ValueModel
+    {
+        public int No { get; set; }
+        public string Value { get; set; }
+
+        public ValueModel(int no)
+        {
+            No = no;
+        }
+        public void Copy(ValueModel model)
+        {
+            No = model.No;
+            Value = model.Value;
+        }
+        public ValueModel Clone()
+        {
+            var other = new ValueModel(No);
+            other.Copy(this);
+            return other;
+        }
+    }
+}
---
+++ RadzenSkeleton/Components/Pages/DataGridCompo.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Radzen;
+using RadzenSkeleton.Components.Models;
+
+namespace RadzenSkeleton.Components.Pages
+{
+    public partial class DataGridCompo
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        public ValueModel[] CouponTrays { get; set; }
+        public ValueModel[] VialTrays { get; set; }
+
+        public DataGridCompo()
+        {
+            CouponTrays = new ValueModel[10];
+            for (int i = 0; i < CouponTrays.Length; i++) {
+                CouponTrays[i] = new ValueModel(i);
+            }
+            VialTrays = new ValueModel[15];
+            for (int i = 0; i < VialTrays.Length; i++) {
+                VialTrays[i] = new ValueModel(i);
+            }
+        }
+    }
+}
